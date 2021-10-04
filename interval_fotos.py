7d9e5980abf2fe3803829de94665de88fffedd0d
SimonHaepers <simon.haepers@gmail.com>
2021-10-04 11:10:15 +0200

    interval_fotos toegevoegd

---
+++ interval_fotos.py
@@ -0,0 +1,16 @@
+from picamera import PiCamera
+from time import sleep
+import numpy
+
+WIDTH = 1600
+HEIGHT = 800
+
+
+with PiCamera() as camera:
+    camera.start_preview()
+    for i in range(1,6):
+        camera.resolution = (WIDTH, HEIGHT)
+        camera.capture('images/imagetest'+str(i)+'.jpg')
+        sleep(1)
+    camera.stop_preview()
+    
