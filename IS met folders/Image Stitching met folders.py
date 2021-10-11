ec88630a179f338581fa5569d2589989e1cc4300
CaptainSSnake <everaert.nolle@gmail.com>
2021-10-11 15:29:12 +0200

    IS door fotos uit folder te halen

--- IS met folders/Image Stitching met folders.py
+++ IS met folders/Image Stitching met folders.py
@@ -12,7 +12,7 @@ for folder in mijnFolders:
     fotos = []
     mijnLijst = os.listdir(pad)
     print(f'Aantal fotos gedecteerd: {len(mijnLijst)}')  # formatted string
-    for foto in fotos:
+    for foto in mijnLijst:
         huidigeFoto = cv2.imread(f'{pad}/{foto}')
         huidigeFoto = cv2.resize(huidigeFoto, (0, 0), None, schaal, schaal)
         fotos.append(huidigeFoto)
@@ -21,9 +21,8 @@ for folder in mijnFolders:
     status, result = stitcher.stitch(fotos)
     if status == cv2.STITCHER_OK:
         print("Panorama gemaakt")
-        cv2.imshow("Gestitchde foto", result)
-        cv2.waitKey(1)
+        cv2.imshow(f"Gestitchde foto: {folder}", result)
     else:
         print("Panorama gefaald")
 
-cv2.waitKey(0)
+cv2.waitKey()
