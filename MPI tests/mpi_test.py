1741caefc822bc59a2529e9e3751d3c3f631182f
SimonHaepers <simon.haepers@gmail.com>
2021-10-11 15:30:04 +0200

    update mpi test

--- MPI tests/mpi_test.py
+++ MPI tests/mpi_test.py
@@ -1,14 +1,13 @@
-from picamera import PiCamera
-from time import sleep
+
 import numpy
 from mpi4py import MPI
 
-WIDTH = 1600
-HEIGHT = 800
+print('test')
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
+print(rank)
 
 if rank == 0:
     data = {'a':7, 'b':5}
@@ -19,8 +18,3 @@ else:
     data = comm.recv(source=0,tag=11)
     print(data)
     
-
-with PiCamera() as camera:
-    camera.start_preview()
-    sleep(5)
-    camera.stop_preview()
