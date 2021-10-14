103f06f4aa509dafaf836b3c26987d081dacd50a
SimonHaepers <simon.haepers@gmail.com>
2021-10-14 16:32:04 +0200

    aanpassingen gemaakt aan IS met folders

--- interval_fotos.py
+++ interval_fotos.py
@@ -10,7 +10,7 @@ with PiCamera() as camera:
     camera.start_preview()
     for i in range(1,6):
         camera.resolution = (WIDTH, HEIGHT)
-        camera.capture('images/imagetest'+str(i)+'.jpg')
+        #camera.capture('images/imagetest'+str(i)+'.jpg')
         sleep(1)
     camera.stop_preview()
     
