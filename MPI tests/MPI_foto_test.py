39f2ea3b76b7dcbd6e70fe40d7ac99ec826caaf7
SimonHaepers <simon.haepers@gmail.com>
2021-10-18 16:14:11 +0200

    camera sync toegevoegd aan MPI fotos test

--- MPI tests/MPI_foto_test.py
+++ MPI tests/MPI_foto_test.py
@@ -1,26 +1,22 @@
 print('test voor import')
 import numpy
-import mpi4py
+#import mpi4py
 from mpi4py import MPI
 
 import cv2
-import os
+#import os
 from picamera import PiCamera
 from time import sleep
 
 print('test na MPI initialisatie')
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-hostname = MPI.Get_processor_name()
+WIDTH = 1600
+HEIGHT = 800
 
 
 def takephotos():
-    WIDTH = 1600
-    HEIGHT = 800
     with PiCamera() as camera:
-        sleep(1)
+        #sleep(1)
         output = numpy.empty((HEIGHT,WIDTH,3),dtype=numpy.uint8)
         camera.resolution = (WIDTH, HEIGHT)
         camera.capture(output, 'rgb')
@@ -43,16 +39,27 @@ def stitch(fotos):
     cv2.waitKey()
             
 def main():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    hostname = MPI.Get_processor_name()
+    
+    #sync voor foto nemen
+    if rank == 0:
+        comm.send(0, dest=1, tag=11)
+    elif rank == 1:
+        comm.recv(source=0, tag=11)
+    
     foto1 = takephotos()
     print('foto genomen from:' + str(hostname))
     if rank == 1:
         comm.Send(foto1,dest=0,tag=13)
         print('foto verzonden from:' + str(hostname))
     elif rank == 0:
-        foto2 = numpy.empty((800,1600,3),dtype=numpy.uint8)
+        foto2 = numpy.empty((HEIGHT,WIDTH,3),dtype=numpy.uint8)
         comm.Recv(foto2, source=1,tag=13)
         print('foto ontvangen')
         stitch([foto1, foto2])
     
-    
-main()
+if __name__ == '__main__':
+    main()
