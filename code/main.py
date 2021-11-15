9b9f52baf034ec48e5d822de6d908bb6d6ec1e9a
SimonHaepers <simon.haepers@gmail.com>
2021-11-15 15:01:45 +0100

    gesynchroniseerde frames voor main.py

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ import numpy as np
 from mpi4py import MPI
 from picamera import PiCamera
 import cv2 as cv
-from time import time
+import time
 
 from stream import Receiver, StreamRecorder, StreamSender
 
@@ -15,28 +15,53 @@ def main():
     rank = comm.Get_rank()
     #size = comm.Get_size()
     hostname = MPI.Get_processor_name()
+    
+    #sync na MPI opstarten
+    if rank == 0:
+        comm.send(0, dest=1, tag=0)
+    elif rank == 1:
+        comm.recv(source=0, tag=0)
+        print('MPI gesynchroniseerd')
+    
 
     with PiCamera(resolution=RESOLUTION, framerate=FRAMERATE) as camera:
         print('start')
-        if rank == 1:
-            camera.start_recording(StreamSender(comm), 'mjpeg')
-            camera.wait_recording(10)
-            camera.stop_recording()
+        frame_count = 1
+        start =  time.perf_counter()
+        while frame_count < 100:
+            #sync voor elke frame
+            if rank == 0:
+                comm.send(0, dest=1, tag=0)
+            elif rank == 1:
+                comm.recv(source=0, tag=0)
+                
+                
+            if rank == 1:
+                #camera.start_recording(StreamSender(comm), 'mjpeg')
+                #camera.wait_recording(10)
+                #camera.stop_recording()
+                current_frame = np.empty((RESOLUTION[1],RESOLUTION[0],3),dtype=np.uint8)
+                camera.capture(current_frame, 'rgb', use_video_port=True)
+                comm.send(current_frame, dest=0, tag=frame_count)
+                
             
-        elif rank == 0:
-            #recorder = StreamRecorder(camera)
-            #camera.start_recording(recorder, 'mjpeg')
-            recv = Receiver(comm)
-            own_image = np.empty((RESOLUTION[1], RESOLUTION[0], 3), dtype=np.uint8)
-            while True:
-                data = recv.read()
-                if data is None:
-                    break
-                print(f'received frame {recv.frame}')
+            elif rank == 0:
+                #recorder = StreamRecorder(camera)
+                #camera.start_recording(recorder, 'mjpeg')
+                #recv = Receiver(comm)
+                own_image = np.empty((RESOLUTION[1], RESOLUTION[0], 3), dtype=np.uint8)
                 camera.capture(own_image, 'rgb', use_video_port=True)
-                print('captured image')
-
-            print('stop')
+                #data = recv.read()
+                received_img = comm.recv(source=1, tag=frame_count)
+                #if data is None:
+                    #break
+                #print(f'received frame {recv.frame}')
+            frame_count +=1
+            print(frame_count)
+        end = time.perf_counter()
+        tijd = end-start
+        print(tijd)
+        print('stop')
 
 
 if __name__ == '__main__':
