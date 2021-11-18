f0757b81adff2c36a0068e602e97f6fad32c8c22
CaptainSSnake <everaert.nolle@gmail.com>
2021-11-18 15:29:06 +0100

    Verplaatsing Code

--- Verzameling van de Codes/Code eerste test IS/Panorama Stitching.py
+++ Verzameling van de Codes/Code eerste test IS/Panorama Stitching.py
@@ -5,9 +5,9 @@ import cv2
 schaal = 1.2
 
 fotos = []
-foto1 = cv2.imread("../../IS met folders/alle fotos/fotos Thuis/Thuis 1.jpg")
+foto1 = cv2.imread("../IS met folders/alle fotos/fotos Thuis/Thuis 1.jpg")
 foto1 = cv2.resize(foto1, (0, 0), None, schaal, schaal)
-foto2 = cv2.imread("../../IS met folders/alle fotos/fotos Thuis/Thuis 2.jpg")
+foto2 = cv2.imread("../IS met folders/alle fotos/fotos Thuis/Thuis 2.jpg")
 foto2 = cv2.resize(foto2, (0, 0), None, schaal, schaal)
 
 fotos.append(foto1)
