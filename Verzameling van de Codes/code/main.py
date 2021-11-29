02bd18c18b36d645dc9bb454bed9008debd2e8be
SimonHaepers <simon.haepers@gmail.com>
2021-11-29 15:19:07 +0100

    voorstelling sender van frame veranderd van bytes naar numpy array

--- Verzameling van de Codes/code/main.py
+++ Verzameling van de Codes/code/main.py
@@ -3,11 +3,23 @@ from mpi4py import MPI
 from picamera import PiCamera
 import cv2 as cv
 from time import time, sleep
+from threading import Thread
 
-from stream import Receiver, StreamRecorder, StreamSender, RESOLUTION, FRAMERATE
+from stream import Receiver, StreamRecorder, StreamSender, RESOLUTION, FRAMERATE, StreamSender2
 
+def senderloop2(camera, comm):
+    with StreamSender2(camera, comm) as sender:
+        camera.start_recording(sender, 'rgb')
+        begin = time()
+        while True:
+            sender.send()
+            if time() - begin > 10:
+                break
+        end = time()
+        print(f'time {end-begin}')
+        camera.stop_recording()
 
-def senderloop(camera):  
+def senderloop(camera, comm):
     sender = StreamSender(comm)
     camera.start_recording(sender, 'mjpeg')
     begin = time()
@@ -21,21 +33,20 @@ def senderloop(camera):
     camera.stop_recording()
         
         
-def receiverloop():
+def receiverloop(camera, comm):
     with StreamRecorder(camera, comm) as recorder:
         camera.start_recording(recorder, 'rgb')
         recv = Receiver(comm)
-        #own_image = np.empty((RESOLUTION[1], RESOLUTION[0], 3), dtype=np.uint8)
         
         while True:
             own_image = recorder.get_frame()
-            sleep(0.01)
+            
             if own_image is not None:
                 data = recv.read()
-                sleep(0.1)
-                #cv.imshow('andere', data)
-                #cv.imshow('eigen', own_image)
-                #cv.waitKey(0)
+                
+#                 t = Thread(target=ShowImages, args = (data,own_image))
+#                 t.start()
+#                 t.join()
                 if data is None:
                     print('stop recording')
                     camera.stop_recording()
@@ -43,19 +54,24 @@ def receiverloop():
                     break
 
 
+def ShowImages(other_image, own_image):
+    cv.imshow('andere', other_image)
+    cv.imshow('eigen', own_image)
+    cv.waitKey(0)
+
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     #size = comm.Get_size()
     hostname = MPI.Get_processor_name()
-
+       
     with PiCamera(resolution=RESOLUTION, framerate=FRAMERATE) as camera:
         print('start')
         comm.Barrier()
         if rank == 1:
-            senderloop(camera)
+            senderloop2(camera, comm)
         elif rank == 0:
-            receiverloop(camera)
+            receiverloop(camera, comm)
 
         print('stop')
 
