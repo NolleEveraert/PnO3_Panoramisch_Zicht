6d57996fdaec87970fb60d4b074ce04b987d9956
SimonHaepers <simon.haepers@gmail.com>
2021-12-22 16:28:51 +0100

    fixed

--- Verzameling van de Codes/code/take_photo.py
+++ Verzameling van de Codes/code/take_photo.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 from picamera import PiCamera
+from time import sleep
 
 RESOLUTION = (2592, 1944)
 
@@ -11,8 +12,11 @@ def main():
     
     with PiCamera(resolution=RESOLUTION) as camera:
         print('start')
+        sleep(5)
         comm.Barrier()
         camera.capture('test.jpg')
             
         print('stop')
     
+if __name__ == '__main__':
+    main()
